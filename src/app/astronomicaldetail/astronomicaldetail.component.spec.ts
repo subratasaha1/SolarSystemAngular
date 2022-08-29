97cae7601ceffeb9cbf7c0124903a990d7b346e0
subrata <ju2005.subrata@gmail.com>
2022-08-29 02:31:27 +0100

    Code changes

--- src/app/astronomicaldetail/astronomicaldetail.component.spec.ts
+++ src/app/astronomicaldetail/astronomicaldetail.component.spec.ts
@@ -1,17 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AstronomicaldetailComponent } from './astronomicaldetail.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RouterModule, Routes } from '@angular/router';
 
 describe('AstronomicaldetailComponent', () => {
   let component: AstronomicaldetailComponent;
   let fixture: ComponentFixture<AstronomicaldetailComponent>;
-
+  let httpMock: HttpTestingController;
+  const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'astronomical-detail/:id', component: AstronomicaldetailComponent }
+  ];
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AstronomicaldetailComponent ]
+      declarations: [ AstronomicaldetailComponent ],
+      imports: [
+        HttpClientTestingModule,
+        RouterModule.forRoot(routes)
+      ]
     })
     .compileComponents();
-
+    httpMock = TestBed.get(HttpTestingController);
     fixture = TestBed.createComponent(AstronomicaldetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/astronomicalsummary/astronomicalsummary.component.spec.ts
+++ src/app/astronomicalsummary/astronomicalsummary.component.spec.ts
@@ -1,28 +1,39 @@
 import { ComponentFixture, TestBed,waitForAsync } from '@angular/core/testing';
 import { AstronomicalsummaryComponent } from './astronomicalsummary.component';
-import { DataService } from '../data.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+//import { DataService } from './data.service';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 describe('AstronomicalsummaryComponent', () => {
   let component: AstronomicalsummaryComponent;
   let fixture: ComponentFixture<AstronomicalsummaryComponent>;
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let httpMock: HttpTestingController;
+  
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [ AstronomicalsummaryComponent ],
-      providers: [DataService]
+      imports: [
+        HttpClientTestingModule,
+        NgxPaginationModule
+      ]
     })
     .compileComponents();
-
+    httpMock = TestBed.get(HttpTestingController);
+  }));
+  
+  beforeEach(() => {
     fixture = TestBed.createComponent(AstronomicalsummaryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  /*it('should create', () => {
+  
+  it('should create', () => {
     expect(component).toBeTruthy();
-  });*/
-  it(`should have a title 'AstronomicalsummaryComponent to display list of solar bodies'`, waitForAsync(() => {
+  });
+  /*it(`should have a title 'AstronomicalsummaryComponent to display list of solar bodies'`, waitForAsync(() => {
     fixture = TestBed.createComponent(AstronomicalsummaryComponent);
     component = fixture.debugElement.componentInstance;
     expect(component.title).toEqual('AstronomicalsummaryComponent to display list of solar bodies');
-  }));
+  }));*/
 });
